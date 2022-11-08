8610716d17f540ca26aecb3e14da3ab6db6d2dba
SheilaNS <shei.nsantos@gmail.com>
2022-11-08 19:46:55 -0300

    install: instalação do tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './index.html' ],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
