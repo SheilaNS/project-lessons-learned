2cd9a3a5a8d6531a67b1ca360eb8f4763b8c1bd1
SheilaNS <shei.nsantos@gmail.com>
2022-11-10 09:03:48 -0300

    fix: voltando como era antes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './src/**/*.{html,js,jsx}' ],
+  content: [ './*.{html,js,jsx}' ],
   theme: {
     fontFamily: {
       sans: ['Mali', 'sans-serif'],
