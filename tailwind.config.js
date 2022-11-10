0b1faf5a924cbb70f95bb5acb3837cdc21a120b3
SheilaNS <shei.nsantos@gmail.com>
2022-11-10 08:59:54 -0300

    refactor: reorganizando os arquivos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './index.html' ],
+  content: [ './src/**/*.{html,js,jsx}' ],
   theme: {
     fontFamily: {
       sans: ['Mali', 'sans-serif'],
